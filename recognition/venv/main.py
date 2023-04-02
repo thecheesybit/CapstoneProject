0ec8af768d343fe2bfeb26b548416005e2385e9b
thecheesybit <ak818ace@gmail.com>
2023-04-02 14:50:17 +0530

    first commit

---
+++ recognition/venv/main.py
@@ -0,0 +1,153 @@
+
+import os
+import pickle
+import cv2
+import face_recognition
+import cvzone
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+import numpy as np
+from datetime import datetime
+
+cred = credentials.Certificate("C:/Users/ak818/Desktop/New folder/recognition/venv/accountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://realtime-ticketing-default-rtdb.firebaseio.com/",
+    'storageBucket': "realtime-ticketing.appspot.com"
+})
+
+bucket = storage.bucket()
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('C:/Users/ak818/Desktop/New folder/recognition/Resources/ticket portal.png')
+
+# Importing the mode images into a list
+folderModePath = r'C:\Users\ak818\Desktop\New folder\recognition\Resources\Modes'
+folderImagePath = r'C:\Users\ak818\Desktop\New folder\recognition\Images'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(len(imgModeList))
+
+# Load the encoding file
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
+
+modeType = 0
+counter = 0
+id = -1
+imgStudent = []
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+    if faceCurFrame:
+        for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print("matches", matches)
+            # print("faceDis", faceDis)
+
+            matchIndex = np.argmin(faceDis)
+            # print("Match Index", matchIndex)
+
+            if matches[matchIndex]:
+                # print("Known Face Detected")
+                # print(studentIds[matchIndex])
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+                imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+                id = studentIds[matchIndex]
+                if counter == 0:
+                    cvzone.putTextRect(imgBackground, "Loading", (275, 400))
+                    cv2.imshow("Face ticket system", imgBackground)
+                    cv2.waitKey(1)
+                    counter = 1
+                    modeType = 1
+
+        if counter != 0:
+
+            if counter == 1:
+                # Get the Data
+                studentInfo = db.reference(f'Students/{id}').get()
+                print(studentInfo)
+                # Get the Image from the storage
+                blob = bucket.get_blob(f'{folderImagePath}/{id}.png')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+                # Update data of attendance
+                datetimeObject = datetime.strptime(studentInfo['last_use_time'], "%Y-%m-%d %H:%M:%S")
+                secondsElapsed = (datetime.now() - datetimeObject).total_seconds()
+                print(secondsElapsed)
+                if secondsElapsed > 30:
+                    ref = db.reference(f'Students/{id}')
+                    studentInfo['UniqueId'] += 1
+                    ref.child('UniqueId').set(studentInfo['UniqueId'])
+                    ref.child('last_use_time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                else:
+                    modeType = 3
+                    counter = 0
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+            if modeType != 3:
+
+                if 10 < counter < 20:
+                    modeType = 2
+
+                imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+                if counter <= 10:
+                    cv2.putText(imgBackground, str(studentInfo['UniqueId']), (861, 125),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['PhoneNo']), (1006, 550),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(id), (1006, 493),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['Gender']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['wallet']), (1025, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['registration']), (1125, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+                    (w, h), _ = cv2.getTextSize(studentInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+                    offset = (414 - w) // 2
+                    cv2.putText(imgBackground, str(studentInfo['name']), (808 + offset, 445),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+                    imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+                counter += 1
+
+                if counter >= 20:
+                    counter = 0
+                    modeType = 0
+                    studentInfo = []
+                    imgStudent = []
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    else:
+        modeType = 0
+        counter = 0
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face ticket system", imgBackground)
+    cv2.waitKey(1)
---
+++ recognition/venv/pushingData.py
@@ -0,0 +1,96 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("C:/Users/ak818/Desktop/New folder/recognition/venv/accountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://realtime-ticketing-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+ 
+data = {
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "PhoneNo": "1234567899",
+            "registration": 2017,
+            "UniqueId": 7,
+            "Gender": "M",
+            "wallet": 2000,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "324732":
+        {
+            "name": "SRK",
+            "PhoneNo": "7376208991",
+            "registration": 1990,
+            "UniqueId": 37,
+            "Gender": "M",
+            "wallet": 2650,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "555555":
+        {
+            "name": "Ayush Kumar",
+            "PhoneNo": "8858903111",
+            "registration": 2023,
+            "UniqueId": 78,
+            "Gender": "M",
+            "wallet": 320,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "565655":
+        {
+            "name": "Kiran Bedi",
+            "PhoneNo": "7758903111",
+            "registration": 1993,
+            "UniqueId": 56,
+            "Gender": "F",
+            "wallet": 3520,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "643222":
+        {
+            "name": "Krish",
+            "PhoneNo": "1212121212",
+            "registration": 2019,
+            "UniqueId": 99,
+            "Gender": "M",
+            "wallet": 2450,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "786333":
+        {
+            "name": "PM",
+            "PhoneNo": "0000111100",
+            "registration": 2014,
+            "UniqueId": 47,
+            "Gender": "M",
+            "wallet": 620,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "PhoneNo": "8877665544",
+            "registration": 2021,
+            "UniqueId": 12,
+            "Gender": "F",
+            "wallet": 8720,
+            "last_use_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "PhoneNo": "88589034444",
+            "registration": 2020,
+            "UniqueId": 27,
+            "Gender": "M",
+            "wallet": 20,
+            "last_use_time": "2022-12-11 00:54:34"
+        }
+}
+ 
+for key, value in data.items():
+    ref.child(key).set(value)
